423fbd5525fe7b01184b084f29c36304accb7def
Russ <45741898+Vulp35@users.noreply.github.com>
2022-11-26 17:24:27 -0600

    Add toggles to mod options

--- src/main/java/space/opendisk/vulp35mod/menu/ModMenuApiImpl.java
+++ src/main/java/space/opendisk/vulp35mod/menu/ModMenuApiImpl.java
@@ -2,12 +2,11 @@ package space.opendisk.vulp35mod.menu;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import space.opendisk.vulp35mod.module.Vulp35Screen;
 
 public class ModMenuApiImpl implements ModMenuApi {
 
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> new Vulp35Screen(parent, null);
+        return screen -> new ModOptionsScreen(new ModOptionsGui(screen));
     }
 }
---
+++ src/main/java/space/opendisk/vulp35mod/menu/ModOptionsScreen.java
@@ -0,0 +1,10 @@
+package space.opendisk.vulp35mod.menu;
+
+import io.github.cottonmc.cotton.gui.GuiDescription;
+import io.github.cottonmc.cotton.gui.client.CottonClientScreen;
+
+public class ModOptionsScreen extends CottonClientScreen {
+    public ModOptionsScreen(GuiDescription description) {
+        super(description);
+    }
+}
